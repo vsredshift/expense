62c83b504961577e0e2de5d93c3d335692fa62ef
Martin Oshaughnessy <vsredshift@gmail.com>
2021-04-26 15:36:49 +0200

    Update package.json

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use('/api/v1/transactions', transactions);
+const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client', 'build')));
@@ -24,5 +25,5 @@ if (process.env.NODE_ENV === 'production') {
     // app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../client/build', 'index.html')));
 }
 
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
